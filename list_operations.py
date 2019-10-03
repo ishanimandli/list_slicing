cae37fcf458a636771467e71ce0285f4135575f1
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-03 13:00:18 -0700

    Second commit, Modified all functions

--- list_operations.py
+++ list_operations.py
@@ -29,7 +29,7 @@ def head(input_list):
 
     """
 
-    return None
+    return input_list[0]
 
 
 def tail(input_list):
@@ -42,7 +42,8 @@ def tail(input_list):
 
     """
 
-    return []
+
+    return input_list[1:]
 
 
 def last(input_list):
@@ -55,7 +56,7 @@ def last(input_list):
 
     """
 
-    return []
+    return input_list[-1]
 
 
 def init(input_list):
@@ -68,7 +69,7 @@ def init(input_list):
 
     """
 
-    return []
+    return input_list[:-1]
 
 
 ##############################################################################
@@ -84,7 +85,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -97,7 +98,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -110,7 +111,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -123,7 +124,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -139,7 +140,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -154,7 +155,8 @@ def replace_head(input_list):
 
     """
 
-    pass
+    input_list[0] = 42
+    return
 
 
 def replace_third_and_last(input_list):
@@ -169,7 +171,9 @@ def replace_third_and_last(input_list):
 
     """
 
-    pass
+    input_list[2] = input_list[-1] = 37
+    # input_list[2], input_list[-1] = [37, 37]
+    return
 
 
 def replace_middle(input_list):
@@ -188,7 +192,8 @@ def replace_middle(input_list):
 
     """
 
-    pass
+    input_list[2:-2] = [42, 37]
+    return
 
 
 def delete_third_and_seventh(input_list):
@@ -203,7 +208,9 @@ def delete_third_and_seventh(input_list):
 
     """
 
-    pass
+    del input_list[2]
+    del input_list[5]
+    return
 
 
 def delete_middle(input_list):
@@ -220,7 +227,8 @@ def delete_middle(input_list):
 
     """
 
-    pass
+    del input_list[2:-2]
+    return
 
 
 ##############################################################################
